dc3d68745162ad001dfbc94578c8a2a62b6e9730
gam6itko <gam6itko@gmail.com>
2022-06-15 09:50:04 +0300

    Column options unsigned

--- src/Annotation/Column.php
+++ src/Annotation/Column.php
@@ -7,6 +7,7 @@ namespace Cycle\Annotated\Annotation;
 use Cycle\ORM\Parser\Typecast;
 use Doctrine\Common\Annotations\Annotation\NamedArgumentConstructor;
 use Doctrine\Common\Annotations\Annotation\Target;
+use JetBrains\PhpStorm\ArrayShape;
 use JetBrains\PhpStorm\ExpectedValues;
 
 /**
@@ -49,6 +50,11 @@ final class Column
         private mixed $default = null,
         private mixed $typecast = null,
         private bool $castDefault = false,
+        /**
+         * @var array Database engine specific options
+         */
+        #[ArrayShape(['unsigned' => 'bool'])]
+        private array $options = [],
     ) {
         if ($default !== null) {
             $this->hasDefault = true;
